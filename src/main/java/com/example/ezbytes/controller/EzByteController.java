c73ce5e161c8efba13a97af12442c2dee3b1a357
udayk <udaykumar1113@gmail.com>
2024-01-19 08:53:43 -0500

    Adding dynamic content to HTML

--- src/main/java/com/example/ezbytes/controller/EzByteController.java
+++ src/main/java/com/example/ezbytes/controller/EzByteController.java
@@ -1,13 +1,15 @@
 package com.example.ezbytes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EzByteController {
 
-    @RequestMapping(value = {"/home","/",""})
-    public String displayHomePage(){
+    @RequestMapping(value={"", "/", "home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "John Doe");
         return "home.html";
     }
 }
