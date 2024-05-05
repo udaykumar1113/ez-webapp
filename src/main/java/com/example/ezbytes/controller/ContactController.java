4a7527ce1aede8e95332b0640018a6f6a2dc0e8b
udayk <udaykumar1113@gmail.com>
2024-05-05 11:23:43 -0400

    Adding Bean validations

--- src/main/java/com/example/ezbytes/controller/ContactController.java
+++ src/main/java/com/example/ezbytes/controller/ContactController.java
@@ -2,11 +2,15 @@ package com.example.ezbytes.controller;
 
 import com.example.ezbytes.model.Contact;
 import com.example.ezbytes.service.ContactService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -22,27 +26,19 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
-    /*@RequestMapping(value = "/saveMsg",method = POST)
-    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
-                                    @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
-        log.info("Name : " + name);
-        log.info("Mobile Number : " + mobileNum);
-        log.info("Email Address : " + email);
-        log.info("Subject : " + subject);
-        log.info("Message : " + message);
-        return new ModelAndView("redirect:/contact");
-    }*/
-
     @RequestMapping(value = "/saveMsg",method = POST)
-    public ModelAndView saveMessage(Contact contact){
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
-
-
-
 }
