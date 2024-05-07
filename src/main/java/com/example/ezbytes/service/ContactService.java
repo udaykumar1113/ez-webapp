3321dbc00d099f3a3235a824e89b6028aedd0df4
udayk <udaykumar1113@gmail.com>
2024-05-07 18:20:36 -0400

    Adding Web bean scopes

--- src/main/java/com/example/ezbytes/service/ContactService.java
+++ src/main/java/com/example/ezbytes/service/ContactService.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 /*
 @Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
@@ -12,8 +15,12 @@ Logger static property in the class at compilation time.
 * */
 @Slf4j
 @Service
+//@RequestScope
+//@SessionScope
+@ApplicationScope
 public class ContactService {
 
+    private int counter = 0;
     private static Logger log = LoggerFactory.getLogger(ContactService.class);
     /**
      * Save Contact Details into DB
@@ -25,6 +32,13 @@ public class ContactService {
         //TODO - Need to persist the data into the DB table
         log.info(contact.toString());
         return isSaved;
+    }
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 
 }
