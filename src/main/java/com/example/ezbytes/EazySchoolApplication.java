f338285d1bf6e32e45b2d9efee768cc780be4548
udayk <udaykumar1113@gmail.com>
2025-01-22 14:15:51 -0500

    Added auditing support in Spring JPA

--- src/main/java/com/example/ezbytes/EazySchoolApplication.java
+++ src/main/java/com/example/ezbytes/EazySchoolApplication.java
@@ -3,11 +3,13 @@ package com.example.ezbytes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.example.ezbytes.repository")
 @EntityScan("com.example.ezbytes.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class EazySchoolApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/ezbytes/service/ContactService.java
+++ src/main/java/com/example/ezbytes/service/ContactService.java
@@ -35,8 +35,6 @@ public class ContactService {
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved = false;
         contact.setStatus(EazySchoolConstants.OPEN);
-        contact.setCreatedBy(EazySchoolConstants.ANONYMOUS);
-        contact.setCreatedAt(LocalDateTime.now());
         Contact savedContact = contactRepository.save(contact);
         if(null != savedContact && savedContact.getContactId()>0) {
             isSaved = true;
@@ -49,13 +47,11 @@ public class ContactService {
         return contactMsgs;
     }
 
-    public boolean updateMsgStatus(int contactId, String updatedBy){
+    public boolean updateMsgStatus(int contactId){
         boolean isUpdated = false;
         Optional<Contact> contact = contactRepository.findById(contactId);
         contact.ifPresent(contact1 -> {
             contact1.setStatus(EazySchoolConstants.CLOSE);
-            contact1.setUpdatedBy(updatedBy);
-            contact1.setUpdatedAt(LocalDateTime.now());
         });
         Contact updatedContact = contactRepository.save(contact.get());
         if(null != updatedContact && updatedContact.getUpdatedBy()!=null) {
